b4f4c3da5414e4772f7d84690edcae3b2d272b18
morzhanov <vlad.morzhanov@gmail.com>
2022-06-06 19:31:09 +0300

    add method to fetch transactions by acc addr

--- main.go
+++ main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
 const (
-	rippleAPIURL                    = "s.altnet.rippletest.net:51233"
+	rippleTestAPIURL                = "s.altnet.rippletest.net:51233"
+	rippleMainAPIURL                = "https://s1.ripple.com:51234/"
 	reqTypeLedger                   = "ledgers"
 	reqTypeTransactions             = "transactions"
+	reqTypeAccountTransactions      = "acc-tx"
 	reqBodyLedgersSubscription      = "{\"id\":\"Example watch for new validated ledgers\",\"command\": \"subscribe\",\"streams\": [\"ledger\"]}"
 	reqBodyTransactionsSubscription = "{\"id\":\"Example watch for new validated transactions\",\"command\": \"subscribe\",\"streams\": [\"transactions\"]}"
 )
@@ -26,6 +32,8 @@ func main() {
 		listenToLedgersUpdates()
 	} else if reqType == reqTypeTransactions {
 		listenToTransactionsUpdates()
+	} else if reqType == reqTypeAccountTransactions {
+		fetchAccountTransactions()
 	} else {
 		log.Fatal(errors.New(fmt.Sprintf("wrong subscription type provided, valid values are %s and %s", reqTypeLedger, reqTypeTransactions)))
 	}
@@ -35,7 +43,7 @@ func listenToLedgersUpdates() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	u := url.URL{Scheme: "wss", Host: rippleAPIURL, Path: "/"}
+	u := url.URL{Scheme: "wss", Host: rippleTestAPIURL, Path: "/"}
 	log.Printf("connecting to %s", u.String())
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
@@ -98,7 +106,7 @@ func listenToTransactionsUpdates() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	u := url.URL{Scheme: "wss", Host: rippleAPIURL, Path: "/"}
+	u := url.URL{Scheme: "wss", Host: rippleTestAPIURL, Path: "/"}
 	log.Printf("connecting to %s", u.String())
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
@@ -154,6 +162,72 @@ func listenToTransactionsUpdates() {
 				msg.Transaction.TransactionType,
 				msg.Transaction.Hash,
 			)
+		}
+	}
+}
+
+func fetchAccountTransactions() {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	t := time.NewTicker(time.Second * 5)
+	txs := make(map[string]bool, 0)
+	txCount := 0
+
+	req := FetchAccTransactionsReqBody{
+		Method: "account_tx",
+		Params: []FetchAccTransactionsParams{{
+			Account:        "rLNaPoKeeBjZe2qs6x52yVPZpZ8td4dc6w",
+			Binary:         false,
+			Forward:        false,
+			LedgerIndexMax: -1,
+			LedgerIndexMin: -1,
+		}},
+	}
+	reqBodyBytes, err := json.Marshal(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		select {
+		case <-interrupt:
+			log.Println("interrupt")
+			return
+		case <-t.C:
+			reqBody := bytes.NewReader(reqBodyBytes)
+			response, err := http.Post(rippleMainAPIURL, "application/json", reqBody)
+			if err != nil {
+				log.Fatal(err)
+			}
+			b, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if response.StatusCode >= 400 {
+				log.Fatal(string(b))
+			}
+			var res FetchAccTransactions
+			if err = json.Unmarshal(b, &res); err != nil {
+				log.Fatal(err)
+			}
+			for _, tx := range res.Result.Transactions {
+				if _, ok := txs[tx.Tx.Hash]; ok {
+					continue
+				}
+				txCount++
+				txs[tx.Tx.Hash] = true
+				log.Printf(
+					"New Transaction: ACC = %s, HASH = %s, LEDGER_IDX = %s, FEE = %d, AMOUNT: %s, TX_TYPE: %s, TX_SIGN: %s",
+					tx.Tx.Account,
+					tx.Tx.Hash,
+					tx.Tx.LedgerIndex,
+					tx.Tx.Fee,
+					tx.Tx.Amount,
+					tx.Tx.TransactionType,
+					tx.Tx.TxnSignature,
+				)
+				log.Println("TX Count = ", txCount)
+			}
 		}
 	}
 }
--- types.go
+++ types.go
@@ -30,29 +30,29 @@ type TransactionSubscriptionRes struct {
 	EngineResultMessage string `json:"engine_result_message"`
 	LedgerHash          string `json:"ledger_hash"`
 	LedgerIndex         int    `json:"ledger_index"`
-	Meta                struct {
-		AffectedNodes []struct {
-			ModifiedNode struct {
-				FinalFields struct {
-					Account    string `json:"Account"`
-					Balance    string `json:"Balance"`
-					Flags      int    `json:"Flags"`
-					OwnerCount int    `json:"OwnerCount"`
-					Sequence   int    `json:"Sequence"`
-				} `json:"FinalFields"`
-				LedgerEntryType string `json:"LedgerEntryType"`
-				LedgerIndex     string `json:"LedgerIndex"`
-				PreviousFields  struct {
-					Balance  string `json:"Balance"`
-					Sequence int    `json:"Sequence"`
-				} `json:"PreviousFields"`
-				PreviousTxnID     string `json:"PreviousTxnID"`
-				PreviousTxnLgrSeq int    `json:"PreviousTxnLgrSeq"`
-			} `json:"ModifiedNode"`
-		} `json:"AffectedNodes"`
-		TransactionIndex  int    `json:"TransactionIndex"`
-		TransactionResult string `json:"TransactionResult"`
-	} `json:"meta"`
+	//Meta                struct {
+	//	AffectedNodes []struct {
+	//		ModifiedNode struct {
+	//			FinalFields struct {
+	//				Account    string `json:"Account"`
+	//				Balance    string `json:"Balance"`
+	//				Flags      int    `json:"Flags"`
+	//				OwnerCount int    `json:"OwnerCount"`
+	//				Sequence   int    `json:"Sequence"`
+	//			} `json:"FinalFields"`
+	//			LedgerEntryType string `json:"LedgerEntryType"`
+	//			LedgerIndex     string `json:"LedgerIndex"`
+	//			PreviousFields  struct {
+	//				Balance  string `json:"Balance"`
+	//				Sequence int    `json:"Sequence"`
+	//			} `json:"PreviousFields"`
+	//			PreviousTxnID     string `json:"PreviousTxnID"`
+	//			PreviousTxnLgrSeq int    `json:"PreviousTxnLgrSeq"`
+	//		} `json:"ModifiedNode"`
+	//	} `json:"AffectedNodes"`
+	//	TransactionIndex  int    `json:"TransactionIndex"`
+	//	TransactionResult string `json:"TransactionResult"`
+	//} `json:"meta"`
 	Status      string `json:"status"`
 	Transaction struct {
 		Account            string `json:"Account"`
@@ -75,4 +75,106 @@ type TransactionSubscriptionRes struct {
 	} `json:"transaction"`
 	Type      string `json:"type"`
 	Validated bool   `json:"validated"`
+}
+
+type FetchAccTransactions struct {
+	Result struct {
+		Account        string `json:"account"`
+		LedgerIndexMax int    `json:"ledger_index_max"`
+		LedgerIndexMin int    `json:"ledger_index_min"`
+		Limit          int    `json:"limit"`
+		Marker         struct {
+			Ledger int `json:"ledger"`
+			Seq    int `json:"seq"`
+		} `json:"marker"`
+		Status       string `json:"status"`
+		Transactions []struct {
+			Meta struct {
+				AffectedNodes []struct {
+					ModifiedNode struct {
+						FinalFields struct {
+							Account      string `json:"Account"`
+							Balance      string `json:"Balance"`
+							Flags        int    `json:"Flags"`
+							MessageKey   string `json:"MessageKey"`
+							OwnerCount   int    `json:"OwnerCount"`
+							Sequence     int    `json:"Sequence"`
+							AccountTxnID string `json:"AccountTxnID,omitempty"`
+							Domain       string `json:"Domain,omitempty"`
+							EmailHash    string `json:"EmailHash,omitempty"`
+							RegularKey   string `json:"RegularKey,omitempty"`
+							TransferRate int64  `json:"TransferRate,omitempty"`
+						} `json:"FinalFields"`
+						LedgerEntryType string `json:"LedgerEntryType"`
+						LedgerIndex     string `json:"LedgerIndex"`
+						PreviousFields  struct {
+							Balance  string `json:"Balance"`
+							Sequence int    `json:"Sequence,omitempty"`
+						} `json:"PreviousFields"`
+						PreviousTxnID     string `json:"PreviousTxnID"`
+						PreviousTxnLgrSeq int    `json:"PreviousTxnLgrSeq"`
+					} `json:"ModifiedNode,omitempty"`
+					DeletedNode struct {
+						FinalFields struct {
+							Account           string `json:"Account"`
+							Balance           string `json:"Balance"`
+							Flags             int    `json:"Flags"`
+							OwnerCount        int    `json:"OwnerCount"`
+							PreviousTxnID     string `json:"PreviousTxnID"`
+							PreviousTxnLgrSeq int    `json:"PreviousTxnLgrSeq"`
+							Sequence          int    `json:"Sequence"`
+						} `json:"FinalFields"`
+						LedgerEntryType string `json:"LedgerEntryType"`
+						LedgerIndex     string `json:"LedgerIndex"`
+						PreviousFields  struct {
+							Balance  string `json:"Balance"`
+							Sequence int    `json:"Sequence"`
+						} `json:"PreviousFields"`
+					} `json:"DeletedNode,omitempty"`
+				} `json:"AffectedNodes"`
+				TransactionIndex  int    `json:"TransactionIndex"`
+				TransactionResult string `json:"TransactionResult"`
+				DeliveredAmount   string `json:"delivered_amount"`
+				DeliveredAmount1  string `json:"DeliveredAmount,omitempty"`
+			} `json:"meta"`
+			Tx struct {
+				Account            string `json:"Account"`
+				Amount             string `json:"Amount,omitempty"`
+				Destination        string `json:"Destination"`
+				DestinationTag     int    `json:"DestinationTag"`
+				Fee                string `json:"Fee"`
+				Flags              int64  `json:"Flags"`
+				LastLedgerSequence int    `json:"LastLedgerSequence"`
+				Sequence           int    `json:"Sequence"`
+				SigningPubKey      string `json:"SigningPubKey"`
+				SourceTag          int    `json:"SourceTag,omitempty"`
+				TransactionType    string `json:"TransactionType"`
+				TxnSignature       string `json:"TxnSignature"`
+				Date               int    `json:"date"`
+				Hash               string `json:"hash"`
+				InLedger           int    `json:"inLedger"`
+				LedgerIndex        int    `json:"ledger_index"`
+			} `json:"tx"`
+			Validated bool `json:"validated"`
+		} `json:"transactions"`
+		UsedPostgres bool `json:"used_postgres"`
+		Validated    bool `json:"validated"`
+		Warnings     []struct {
+			Id      int    `json:"id"`
+			Message string `json:"message"`
+		} `json:"warnings"`
+	} `json:"result"`
+}
+
+type FetchAccTransactionsReqBody struct {
+	Method string                       `json:"method"`
+	Params []FetchAccTransactionsParams `json:"params"`
+}
+
+type FetchAccTransactionsParams struct {
+	Account        string `json:"account"`
+	Binary         bool   `json:"binary"`
+	Forward        bool   `json:"forward"`
+	LedgerIndexMax int    `json:"ledger_index_max"`
+	LedgerIndexMin int    `json:"ledger_index_min"`
 }
